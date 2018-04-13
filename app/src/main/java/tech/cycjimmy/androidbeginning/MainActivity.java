840c2fe6b5901c23de20193cd09bb560a3f121fb
cycjimmy <cycjimmy@gmail.com>
2018-04-13 17:04:47 +0800

    try coding

--- app/src/main/java/tech/cycjimmy/androidbeginning/MainActivity.java
+++ app/src/main/java/tech/cycjimmy/androidbeginning/MainActivity.java
@@ -8,13 +8,20 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.MultiAutoCompleteTextView;
+import android.widget.RadioGroup;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView imageView;
+    private CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +33,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // 测试点击事件
         Button myButton = findViewById(R.id.button);
         ImageButton myImageButton = findViewById(R.id.image_button);
-        ImageView myImage = findViewById(R.id.image);
+        imageView = findViewById(R.id.image);
 
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,20 +50,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        myImage.setOnClickListener(this);
+        imageView.setOnClickListener(this);
 
-        // 测试 AutoCompleteTextView
+        // toggleButton
+        ToggleButton toggleButton = findViewById(R.id.toggleButton);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    imageView.setVisibility(View.VISIBLE);
+                } else {
+                    imageView.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        // CheckBox
+        checkBox = findViewById(R.id.checkBox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.i("tag", isChecked + "");
+
+                if (isChecked) {
+                    String text = checkBox.getText().toString();
+                    Log.i("tag", "onCheckedChanged: " + text);
+                }
+            }
+        });
+
+        // RadioGroup & RadioButton
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radio1:
+                        Log.i("tag", "Radio1");
+                        break;
+
+                    case R.id.radio2:
+                        Log.i("tag", "Radio2");
+                        break;
+                }
+            }
+        });
+
+
+        // AutoCompleteTextView
         AutoCompleteTextView myAutoCompleteTextView = findViewById(R.id.autoCompleteTextView);
-
         String[] res = {"beijing", "tianjin", "nanjing", "shanghai", "shanghai2"};    // 数据源
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(    // 适配器
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(    // 适配器
                 this,
                 android.R.layout.simple_list_item_1,
                 res
         );
         myAutoCompleteTextView.setAdapter(arrayAdapter);                 // 绑定适配器
 
-        // 测试 MultiAutoCompleteTextView
+        // MultiAutoCompleteTextView
         MultiAutoCompleteTextView multiAutoCompleteTextView = findViewById(R.id.multiAutoCompleteTextView);
         multiAutoCompleteTextView.setAdapter(arrayAdapter);
         multiAutoCompleteTextView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer()); // 设置分隔符: 逗号
